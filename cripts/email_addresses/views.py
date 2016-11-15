7738e1e06b297b1961b92a6c536c2a0d2df352bc
Matt Weir <cweir@mitre.org>
2016-11-15 17:02:47 -0500

    File upload for email addresses now works
    Also added support for basic searching for emails. Getting ready to
    start adding support for datasets

--- cripts/email_addresses/views.py
+++ cripts/email_addresses/views.py
@@ -17,8 +17,8 @@ from cripts.core.data_tools import json_handler
 
 from cripts.email_addresses.handlers import email_address_add_update, get_email_address_details
 from cripts.email_addresses.handlers import generate_email_address_jtable, generate_email_address_csv
-from cripts.email_addresses.handlers import process_bulk_add_email_addresses
-from cripts.email_addresses.forms import EmailAddressForm
+from cripts.email_addresses.handlers import process_bulk_add_email_addresses, handle_email_list_file
+from cripts.email_addresses.forms import EmailAddressForm, UploadEmailAddressForm
 
 @user_passes_test(user_can_view_data)
 def email_addresses_listing(request,option=None):
@@ -118,34 +118,43 @@ def bulk_add_email_addresses(request):
 @user_passes_test(user_can_view_data)
 def bulk_file_upload_email_addresses(request):
     """
-    Bulk add Email Addresses via a file upload.
-    Args:
-        request: The Django context which contains information about the
-            session and key/value pairs for the bulk add Email Addresses request
-    Returns:
-        If the request is not a POST and not a Ajax call then:
-            Returns a rendered HTML form for a bulk add of IPs
-        If the request is a POST and a Ajax call then:
-            Returns a response that contains information about the
-            status of the bulk uploaded Email Addresses. This may include information
-            such as Email Address that failed or successfully added. This may
-            also contain helpful status messages about each operation.
+    Bulk upload of Email Addresses via a file upload to CRIPTs.
+    :param request: Django request object (Required)
+    :type request: :class:`django.http.HttpRequest`
+    :returns: :class:`django.http.HttpResponse`
     """
+    
+    if request.method == 'POST':
+        form = UploadEmailAddressForm(request.user, request.POST, request.FILES)
+        if form.is_valid():
+            cleaned_data = form.cleaned_data
+            filedata = request.FILES['filedata']
+            filename = filedata.name
+            data = filedata.read() # XXX: Should be using chunks here.
+            source = cleaned_data.get('source')
+            method = cleaned_data.get('method')
+            user = request.user.username
 
-    formdict = form_to_dict(EmailAddressForm(request.user, None))
-
-    if request.method == "POST" and request.is_ajax():
-        response = process_bulk_add_email_addresses(request, formdict)
-
-        return HttpResponse(json.dumps(response,
-                            default=json_handler),
-                            content_type="application/json")
+            status = handle_email_list_file(filename, data, source, user,
+                                     method=method)
+            if status['success']:
+                return render_to_response('file_upload_response.html',
+                                          {'response': json.dumps({'message': 'Email list uploaded successfully!<br>' + status['message']}), 'success': True},
+                                          RequestContext(request))
+            else:
+                return render_to_response('file_upload_response.html',
+                                          {'response': json.dumps({ 'success': False,'message': status['message']})}
+                                          , RequestContext(request))
+            
+        else:
+            return render_to_response('file_upload_response.html',
+                                      {'response': json.dumps({'success': False,
+                                                               'form': form.as_table()})},
+                RequestContext(request))
     else:
-        return render_to_response('bulk_add_default.html', {'formdict': formdict,
-                                                            'title': "Bulk Add Email Addressess",
-                                                            'table_name': 'email_address',
-                                                            'local_validate_columns': [form_consts.EmailAddress.EMAIL_ADDRESS],
-                                                            'is_bulk_add_objects': True}, RequestContext(request)) 
+        return render_to_response('error.html',
+                                  {'error': "Expected POST." + str(request.method)},
+                                  RequestContext(request))
 
  
 @user_passes_test(user_can_view_data)
