8a5e631e52296c58591e4a7c68bb26644e394b07
Matt Weir <cweir@mitre.org>
2016-11-16 17:09:29 -0500

    Started adding support for datasets
    Just adding stub support for uploading a dataset. I need to spend some
    time sitting down and figuring out what data I actually want to save for
    them. Also hash support will come later. I know, I'm leaving the most
    important feature til last, but datasets will be the main way of
    organizing how hashes are saved/viewed

--- cripts/datasets/urls.py
+++ cripts/datasets/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls import url
 urlpatterns = [
     url(r'^list/$', 'datasets_listing', prefix='cripts.datasets.views'),
     url(r'^list/(?P<option>\S+)/$', 'datasets_listing', prefix='cripts.datasets.views'),
+    url(r'^upload/$', 'upload_dataset', prefix='cripts.datasets.views'),
 ]
